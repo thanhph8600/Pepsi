87e316ec5619600fdc0eec58c22f3719e7b4278e
thanhph8600 <pht456654@gmail.com>
2023-02-10 10:30:21 +0700

    done

--- js/main.js
+++ js/main.js
@@ -10,3 +10,108 @@ elementss[0].addEventListener('click', function() {
     var nav = document.getElementById('block-nav__content')
     nav.style.display = 'none'
 }, false);
+
+
+
+$(document).ready(function(e){
+
+    dealerObj.forEach((i)=>loadDealer(i))
+
+    // $('.first-section__btn__number').click(function() {
+    //    // console.log($(this));
+    //     console.log( $('.first-section__btn__number').index(this) );
+    //     $('.first-section__number').css("display", "none");
+    //     $('.first-section__number').eq($('.first-section__btn__number').index(this)).css("display", "block");
+    // });
+});
+
+function loadDealer(data) {
+    //console.log(data)
+    var div = document.createElement("div")
+    div.classList.add("first-section__content__boxs")
+    div.innerHTML= `                
+            <div class="first-section__content__boxs__img">
+                <img src="`+data.img+`" alt="">
+            </div>
+            <div class="first-section__content__boxs__info">
+                <div class="">
+                    <h3 class="first-section__content__boxs__name">`+data.name+`</h3>
+                    <p class="first-section__content__boxs__time">Oppen: `+data.timeOppen+` - <span>`+data.timeLose+`</span></p>
+                    <p class="firts-section__content__boxs__dress">`+data.dress+`</p>
+                </div>
+
+                <div class="first-section__btn">
+                    <p class="btn south first-section__btn__item first-section__btn__number" onclick="viewNumber(this)">VIEW NUMBER</p>
+                    <a href="https://www.google.com/maps" class="btn north first-section__btn__item first-section__btn__map">VIEW MAP</a>
+                </div>
+            </div>
+            <div class="first-section__number" style="display:none" >
+                        <p>${data.number}</p>
+                        <span class="close-number">&times;</span>
+            </div>`
+    document.getElementById("north-content").appendChild(div)
+}
+
+function south(data_id) {
+
+    document.getElementById("north-content").innerHTML = "";
+
+    if(data_id==0){
+        dealerObj.forEach((i)=>loadDealer(i))
+    }
+
+   //console.log(dealerObj)
+   var objectFilter = [];
+   //dealerObj.fi
+  
+   objectFilter = dealerObj.filter(obj => obj.area == data_id);
+  // console.log(objectFilter);
+ 
+   objectFilter.map((items, key) => {
+
+
+    var div = document.createElement("div")
+    div.classList.add("first-section__content__boxs")
+    div.innerHTML= `                
+            <div class="first-section__content__boxs__img">
+                <img src="${items.img}" alt="">
+            </div>
+            <div class="first-section__content__boxs__info">
+                <div class="">
+                    <h3 class="first-section__content__boxs__name">`+items.name+`</h3>
+                    <p class="first-section__content__boxs__time">Oppen: `+items.timeOppen+` - <span>`+items.timeLose+`</span></p>
+                    <p class="firts-section__content__boxs__dress">`+items.dress+`</p>
+                </div>
+
+                <div class="first-section__btn">
+                    <p class="btn south first-section__btn__item first-section__btn__number" onclick="viewNumber(this)">VIEW NUMBER</p>
+                    <a href="https://www.google.com/maps" class="btn north first-section__btn__item first-section__btn__map">VIEW MAP</a>
+                </div>
+            </div>
+            <div class="first-section__number" style="display:none" >
+                        <p>${items.number}</p>
+                        <span class="close-number">&times;</span>
+            </div>`
+    document.getElementById("north-content").appendChild(div);
+   });
+}
+
+
+function viewNumber(i) {
+    var numbers = document.getElementsByClassName("first-section__number")
+    for(var j=0;j<numbers.length;j++){
+        numbers[j].style.display = "none"
+    }
+    var openNumber = i.parentElement.parentElement.parentElement
+    var number = openNumber.getElementsByClassName("first-section__number")[0]
+    number.style.display = "block"
+    number.getElementsByClassName("close-number")[0].addEventListener("click",function() {
+        number.style.display = "none"
+     })
+   
+}
+
+
+    
+
+
---
+++ js/product.js
@@ -0,0 +1,150 @@
+var elements = document.getElementsByClassName("icon-open-nav");
+elements[0].addEventListener('click', function() {
+    var nav = document.getElementById('block-nav__content')
+    nav.style.display = 'block'
+}, false);
+
+var elementss = document.getElementsByClassName("close-icon");
+elementss[0].addEventListener('click', function() {
+    //block-nav__content
+    var nav = document.getElementById('block-nav__content')
+    nav.style.display = 'none'
+}, false);
+
+
+
+$(document).ready(function(e){
+
+    for(var i=0;i<10;i++){
+        // console.log(productObj[i])
+        loadDealer(productObj[i],i)
+    }
+
+    // remover()
+
+});
+
+
+function loadDealer(data,index) {
+    //console.log(data)
+    var div = document.createElement("div")
+    div.classList.add("product-content__item__box")
+    div.innerHTML= `                
+            <div class="img">
+                <img src="${data.img}" alt="">
+            </div>
+            <div class="product-content__item__box__name">
+                <p>${data.name}</p>
+            </div>
+            <div class="product-content__item__box__hover" >
+                <i class="fa fa-search" aria-hidden="true" onclick="oppenPopup(${index})"></i>
+            </div>
+            <div class="product-content__item__box__new">
+            <img src="${data.new}" alt="">
+            </div>
+            `
+
+    document.getElementById("product-item").appendChild(div)
+    // var news = document.getElementsByClassName("product-content__item__box__new_p")[0]
+    // console.log(news)
+    // if(news==''){
+    //     news
+    // }
+}
+
+// function remover() {
+//     var div = document.getElementsByClassName("product-content__item__box")
+//     var checkNew = document.getElementsByClassName("product-content__item__box__new_p")
+//     for(var i=0;i<div.length;i++){
+//         if(checkNew[i].innerHTML==""){
+//             document.getElementsByClassName("product-content__item__box__new")[i].style.display="none"
+//         }
+//     }
+// }
+
+function oppenPopup(index) {
+    
+    popup = document.getElementsByClassName("product-popup")[0]
+    popup.innerHTML = `
+                <div class="product-popup__img">
+                    <img src="${productObj[index].img}" alt="">
+                </div>
+                <div class="product-popup__info">
+                    <div class="product-popup__info__top">
+                        <h2>${productObj[index].name}</h2>
+                        <P>${productObj[index].content}
+                        </P>
+                    </div>
+                    
+                    <div class="product-popup__info__top__bottom">
+                        <p>Origin: VietNam</p>
+                        
+                    </div>
+                </div>
+                <div class="close-popup" onclick="losePopup()">&times;</div>
+                `
+    popup.style.display = "flex"
+    document.getElementsByClassName("over-layer")[0].style.display = "block"
+    document.getElementsByClassName("over-layer")[0].addEventListener("click",function(){
+        losePopup()
+    })
+}
+
+
+function losePopup() {
+    document.getElementsByClassName("over-layer")[0].style.display = "none"
+    document.getElementsByClassName("product-popup")[0].style.display = 'none'
+}
+
+
+function showMore() {
+
+    for(var i=10;i<20;i++){
+        loadDealer(productObj[i],i)
+    }
+    
+                
+    document.getElementsByClassName("showMore")[0].getElementsByTagName("span")[0].style.display ="none"
+    document.getElementsByClassName("showMore")[0].getElementsByTagName("span")[1].style.display =""
+}
+
+
+function showAll() {
+    for(var i=20;i<productObj.length;i++){
+        loadDealer(productObj[i],i)
+    }
+    document.getElementsByClassName("showMore")[0].getElementsByTagName("span")[1].style.display ="none"
+}
+
+
+
+function filter(type) {
+    document.getElementById("product-item").innerHTML=""
+    console.log(type)
+    if(type=="All"){
+        document.getElementsByClassName("showMore")[0].style.display = "block"
+        for(var i=0;i<10;i++){
+            loadDealer(productObj[i],i)
+        }
+        document.getElementsByClassName("showMore")[0].getElementsByTagName("span")[0].style.display ="block"
+
+    }else{
+        document.getElementsByClassName("showMore")[0].style.display = "none"
+        for(var i=0;i<productObj.length;i++){
+            if(productObj[i].type==type) {
+                loadDealer(productObj[i],i)
+            }else{
+                continue;
+            }
+        }
+    }
+}
+
+function getValue(obj) {
+    var options = obj.children
+    for(var i=0;i<options.length;i++) {
+        if(options[i].selected){
+            filter(options[i].value)
+        }
+    }
+}
